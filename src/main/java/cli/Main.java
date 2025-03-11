fbc83533089dc6bc5940e436cd61d6667ec4a556
BogruAKVD <bnmmnb456654@inbox.ru>
2025-03-11 10:13:38 +0300

    Init java gradle project with ci

---
+++ src/main/java/cli/Main.java
@@ -0,0 +1,11 @@
+package cli;
+
+public class Main {
+    public static String HelloWorld() {
+        return "HelloWorld";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(HelloWorld());
+    }
+}
