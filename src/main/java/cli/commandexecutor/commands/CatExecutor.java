1dda6d9cb6a266ccee0c2804976a926f4fa7f69f
yekaterinika <yekaterina.nikolaeva2004@yandex.ru>
2025-03-17 01:22:28 +0300

    Implement cat and pwd (without comments)

--- src/main/java/cli/commandexecutor/commands/CatExecutor.java
+++ src/main/java/cli/commandexecutor/commands/CatExecutor.java
@@ -2,11 +2,33 @@ package cli.commandexecutor.commands;
 
 import cli.model.CommandResult;
 
+import java.io.IOException;
+import java.nio.file.Files;
+
+import java.nio.file.Path;
 import java.util.List;
 
 public class CatExecutor implements InternalCommandExecutor {
+    private final static String helpMessage = "Get files' content";
+    private final static String flagHelpMessage = "help";
+
     @Override
     public CommandResult execute(List<String> args, List<String> flags) {
-        return null;
+        if (flags.contains(helpMessage)) {
+            return new CommandResult(0, flagHelpMessage);
+        }
+        if (args.isEmpty()) {
+            return new CommandResult(1, "cat: files are not specified");
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String file : args) {
+            Path filePath = Path.of(file);
+            try {
+                stringBuilder.append(Files.readString(filePath));
+            } catch (IOException e) {
+                return new CommandResult(1, "cat: cannot read file " + file);
+            }
+        }
+        return new CommandResult(0, stringBuilder.toString());
     }
 }
