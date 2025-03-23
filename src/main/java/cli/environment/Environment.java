c0fe8f6da993faca14b32785be3dace652e18420
BogruAKVD <bnmmnb456654@inbox.ru>
2025-03-23 04:35:57 +0300

    Add comments to Environment and PipelineExecutor

--- src/main/java/cli/environment/Environment.java
+++ src/main/java/cli/environment/Environment.java
@@ -1,6 +1,23 @@
 package cli.environment;
 
+/**
+ * The Environment interface defines the contract for managing environment variables.
+ * Implementations of this interface are responsible for storing and retrieving variable values.
+ */
 public interface Environment {
+    /**
+     * Retrieves the value of a specified environment variable.
+     *
+     * @param name The name of the variable to retrieve.
+     * @return The value of the variable, or null if it does not exist.
+     */
     String getVariable(String name);
+
+    /**
+     * Sets the value of a specified environment variable.
+     *
+     * @param name  The name of the variable to set.
+     * @param value The new value for the variable.
+     */
     void setVariable(String name, String value);
 }
